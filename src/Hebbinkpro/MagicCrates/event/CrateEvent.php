82fc5af4a1c1bdd1983d361fa3bb9be95861eece
Hebbinkpro <jorim.hebbink@gmail.com>
2024-02-03 12:24:04 +0100

    Update copyright

--- src/Hebbinkpro/MagicCrates/event/CrateEvent.php
+++ src/Hebbinkpro/MagicCrates/event/CrateEvent.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ *   __  __             _       _____           _
+ *  |  \/  |           (_)     / ____|         | |
+ *  | \  / | __ _  __ _ _  ___| |     _ __ __ _| |_ ___  ___
+ *  | |\/| |/ _` |/ _` | |/ __| |    | '__/ _` | __/ _ \/ __|
+ *  | |  | | (_| | (_| | | (__| |____| | | (_| | ||  __/\__ \
+ *  |_|  |_|\__,_|\__, |_|\___|\_____|_|  \__,_|\__\___||___/
+ *                 __/ |
+ *                |___/
+ *
+ * Copyright (c) 2024 Hebbinkpro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 
 namespace Hebbinkpro\MagicCrates\event;
 
